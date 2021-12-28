2de3abb8b623bcfb26da4d720fabb49dfd7a0b65
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2021-12-28 05:49:27 +0530

    adding solutions

--- CSES/Repetitions.cpp
+++ CSES/Repetitions.cpp
@@ -1,15 +1,36 @@
 /**
  *    author:   abhijayrajvansh
- *    created:  28.12.2021 04:59:42
+ *    created:  28.12.2021 05:18:18
 **/
 #include <bits/stdc++.h>
-
 using namespace std;
-#define nl '\n'
 
 int main (){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    string s;
+    cin >> s;
+    map<char, int> freq;
+    for(int i = 0; i < s.size(); i++){
+        freq[s[i]]++;
+    }
+    /*-------------------------------------------------
+    map<char, int> :: iterator it;
+    for(it = freq.begin(); it != freq.end(); it++){
+        cout << it->first << " " << it->second << endl;
+    } 
+    bit long process to traverse...
+    also can be done using for each loop with "auto" keyword
+
+    for(auto it = freq.begin(); it != freq.end(); it++){
+        cout << it->first << " appeared " << it->second << " times" << '\n';
+    }
+    --------------------------------------------------*/
+    // printing the highest value:
+    int ans = 0;
+    for(auto it = freq.begin(); it != freq.end(); it++){
+        ans = max(ans, it->second);
+    }
+    cout << ans;
     return 0;
 }
